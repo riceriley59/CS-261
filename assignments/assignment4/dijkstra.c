b68bcc8ca6bb007255f9b1900281c39a9a26cf14
Riley Rice <riceriley59@gmail.com>
2023-02-25 21:04:40 -0800

    debugging

--- assignments/assignment4/dijkstra.c
+++ assignments/assignment4/dijkstra.c
@@ -16,6 +16,55 @@
 #define START_NODE 0
 #define INF 999
 
+struct dijNode{
+	int current;
+	int prev;
+};
+
+void dijkstra(int start, int adj[][10]){
+	int cost[10], prev[10];
+	struct pq* pq = pq_create();
+
+	for(int i = 0; i < 10; i++){
+		cost[i] = INF;
+		prev[i] = -1;
+	}
+
+	struct dijNode first;
+	first.current = start;
+	first.prev = prev[start];
+
+	pq_insert(pq, (void*)&first, 0);
+
+	while(!pq_isempty(pq)){
+		int c = pq_first_priority(pq);
+
+		struct dijNode* popped = pq_remove_first(pq);
+
+		if(c < cost[popped->current]){
+			cost[popped->current] = c;
+			prev[popped->current] = popped->prev;
+
+			for(int i = 0; i < 10; i++){
+				if(adj[popped->current][i] > 0){
+					int neighborCost = adj[popped->current][i];
+
+					first.current = i;
+					first.prev = popped->current;
+
+					pq_insert(pq, (void*)&first, (neighborCost + c));
+				}
+			}
+		}
+	}
+
+	for(int i = 0; i < 10; i++){
+		printf("The cost for each node: %d\n", cost[i]);
+	}
+
+	pq_free(pq);
+}
+
 int main(int argc, char const *argv[]) {
 	/*
 	 * Open file and read the first two int: num of nodes, num of edges
@@ -36,21 +85,12 @@ int main(int argc, char const *argv[]) {
 		int source, dest, weight;
 
 		fscanf(file, "%d %d %d", &source, &dest, &weight);
-
 		adjacency[source][dest] = weight;
 	}
 
-/* 	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			printf("%d  ", adjacency[i][j]);
-		}
-		printf("\n");
-	} */
-
-	
+	dijkstra(START_NODE, adjacency);
 
 	fclose(file);
-
-	 
-  return 0;
+ 
+ 	return 0;
 }
